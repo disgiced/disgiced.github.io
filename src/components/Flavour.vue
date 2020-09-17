5c38679442d196cfcaf5c2695e2fbe4167b742a0
projsh <projsh@users.noreply.github.com>
2020-09-17 16:33:52 +1000

    changes

--- src/components/Flavour.vue
+++ src/components/Flavour.vue
@@ -10,6 +10,14 @@
         <img draggable="false" class="bottle-part" :src="'/static/' + flavour + '.png'"/>
       </div>
     </section>
+    <section class="wider sustainable" :class="flavour + '-bg'">
+      <div class="inner left">
+        <div class="titles">
+          <div class="main-title normal">100% sustainable</div>
+          <div class="title-sub normal">Our {{ flavours[flavour].ingredient }} comes from 100% sustainable sources, so you can be sure you're not contributing to your carbon footprint. We care about the environment, just like you! Thank you for looking after our planet.</div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -25,32 +33,38 @@ export default {
         chocolate: {
           name: 'Iced Chocolate',
           desc: `Made using 100% sustainable cocoa, our Iced Chocolate flavour is unlike anything in the market.
-              Enjoy a sweet, creamy, rich chocolate flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich chocolate flavour with our range of alternative milks!`,
+          ingredient: 'cocoa'
         },
         coffee: {
           name: 'Iced Coffee',
           desc: `Sourced from 100% sustainable farms, our Arabica coffee beans truly blend in beautifuly to create a unique Iced Coffee flavour.
-              Enjoy a sweet, creamy, rich coffee flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich coffee flavour with our range of alternative milks!`,
+          ingredient: 'coffee beans'
         },
         caramel: {
           name: 'Caramel',
           desc: `Have a sweet tooth? You'll love our unique caramel flavour, made using 100% sustainable sugars!
-              Enjoy a sweet, creamy, rich caramel flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich caramel flavour with our range of alternative milks!`,
+          ingredient: 'sugar'
         },
         strawberry: {
           name: 'Strawberry',
           desc: `Craving a sweet berry flavour? Our strawberry flavour will satisfy you, using 100% Australian strawberries from 100% sustainable farms.
-              Enjoy a sweet, creamy, rich strawberry flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich strawberry flavour with our range of alternative milks!`,
+          ingredient: 'strawberries'
         },
         vanilla: {
           name: 'Vanilla',
           desc: `Looking for something mild but pleasantly sweet? Try our vanilla flavour, sourced from 100% sustainable vanilla beans!
-              Enjoy a sweet, creamy, rich vanilla flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich vanilla flavour with our range of alternative milks!`,
+          ingredient: 'vanilla'
         },
         mocha: {
           name: 'Mocha',
           desc: `Want a smooth blend of coffee and chocolate? Look no further than our mocha flavour! Using 100% sustainable cocoa and Arabica coffee beans, you'll be sure to love the combination.
-              Enjoy a sweet, creamy, rich mocha flavour with our range of alternative milks!`
+              Enjoy a sweet, creamy, rich mocha flavour with our range of alternative milks!`,
+          ingredient: 'cocoa and coffee beans'
         }
       }
     }
@@ -85,6 +99,7 @@ section.title p {
 .bottle-img {
   display: flex;
   align-items: flex-end;
+  transform: translateX(50px);
 }
 .bottle {
   width: 500px;
@@ -94,5 +109,74 @@ section.title p {
   position: absolute;
   width: 200px;
   transform: translateX(60px);
+}
+.inner {
+  max-width: 1200px;
+  width: 100%;
+  padding: 50px 25px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 300px;
+}
+.right {
+  background-position: right;
+  align-items: flex-start;
+  text-align: left;
+}
+.left {
+  background-position: left;
+  align-items: flex-end;
+  text-align: right;
+}
+.inner .title-sub.normal {
+  font-size: 18px;
+  margin-top: 25px;
+  max-width: 640px;
+}
+.inner .main-title.normal {
+  font-size: 56px;
+  font-weight: 900;
+}
+.wider {
+  width: 100%;
+}
+.chocolate-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/cocoa.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.coffee-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/coffeebeans.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.mocha-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/coffeebeans.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.caramel-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/sugarcane.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.strawberry-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/strawberries.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.vanilla-bg {
+  background-image: linear-gradient(to left, rgba(0,0,0,.85), rgba(0,0,0,.3)), url('../assets/vanilla.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
 }
 </style>
